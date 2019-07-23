21b523c9125e4db6edcea9149eedd17c5fd77d4b
Krzysztof Majewski <kmajewski@berg.technology>
2019-07-23 14:07:41 +0200

    Polling non blocking server

---
+++ src/com/majewski/server/SingleThreadedPollingNonBLockingServer.java
@@ -0,0 +1,33 @@
+package com.majewski.server;
+
+import com.majewski.handlers.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Executors;
+
+class SingleThreadedPollingNonBLockingServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress(8080));
+        ssc.configureBlocking(false);
+        UncheckedIOExceptionConverterHandler<SocketChannel> handler = new UncheckedIOExceptionConverterHandler<>(
+                new TransmogrifyChannelHandler());
+        Collection<SocketChannel> sockets = new ArrayList<>();
+        while (true) {
+            SocketChannel sc = ssc.accept(); // almost always null
+            if (sc != null) {
+                sockets.add(sc);
+                System.out.println("Connected to " + sc);
+                sc.configureBlocking(false);
+            }
+            sockets.stream().filter(SocketChannel::isConnected).forEach(handler::handle);
+            sockets.removeIf(socket -> !socket.isConnected());
+        }
+    }
+
+}
